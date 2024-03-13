781a3315934a48c6c777ea15e0fcb916a789495b
Josyway <107008079+Josyway@users.noreply.github.com>
2024-03-12 21:17:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("form-validator");
+
+
+form.addEventListener('submit', function (e) {
+	e.preventDefault();
+    
+
+    const primeiroNumero = document.getElementById("primeiro_numero");
+    const segundoNumero = document.getElementById("segundo_numero");
+    const mensagemSucesso = `O numero: <b>${primeiro_numero.value}</b> é menor que:  <b>${segundo_numero.value}</b>`;
+    const mensagemErro = `O segundo numero precasa ser maior que o primeiro numero`;
+    
+
+    if (segundoNumero.value > primeiroNumero.value) {
+        const containerMensagemSucesso = document.querySelector(".success-message");
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+
+        primeiroNumero.value = '';
+        segundoNumero.value = '';
+    }else{ 
+        const containerMensagemErro = document.querySelector(".error-message");
+        containerMensagemErro.innerHTML = mensagemErro;
+        containerMensagemErro.style.display = 'block';
+
+        }
+    })
+    console.log(form);
+    
